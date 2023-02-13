640a796b2d8b95aa16ec8d417d4a8ec8c92cc78a
Stephen Zhang <syz@syz.id.au>
2023-02-13 13:54:00 +1100

    linear-test and shared nodes

--- scripts/visutil.py
+++ scripts/visutil.py
@@ -3,7 +3,7 @@ import networkx as nx
 import itertools
 import matplotlib
 
-def draw(g, gene_names, thresh = None, lst = None, cmap_dict = {0 : "MyGrey", 1 : "MyGrey"}, layout_args = ""):
+def draw(g, gene_names, thresh = None, node_list = None, edge_list = None, pos = None, cmap_dict = {0 : "MyGrey", 1 : "MyGrey"}, layout_args = "", draw = True):
     # get weights from g
     edges,weights = zip(*nx.get_edge_attributes(g,'weight').items())
     weights = np.array(list(weights))
@@ -11,21 +11,30 @@ def draw(g, gene_names, thresh = None, lst = None, cmap_dict = {0 : "MyGrey", 1 
     # node and edgelist
     nl = list(g.nodes())
     el = list(g.edges())
+    # subset edges
     if thresh is not None:
         edge_idx = np.where(weights > np.quantile(weights, thresh))[0]
         edgelist = [el[i] for i in edge_idx]
-    elif lst is not None:
-        edgelist = [tuple(i) for i in lst]
-    node_idx = list(set(itertools.chain(*edgelist)))
-    nodelist = [nl[i] for i in node_idx]
+    elif edge_list is not None:
+        edgelist = [tuple(i) for i in edge_list]
+    # subset nodes
+    if node_list is not None:
+        nodelist = node_list
+        edgelist = [e for e in edgelist if ((e[0] in nodelist) and (e[1] in nodelist))]
+    else:
+        node_idx = list(set(itertools.chain(*edgelist)))
+        nodelist = [nl[i] for i in node_idx]
     # take subgraph
     g_sub = g.subgraph(nodelist)
+    if draw == False:
+        return g_sub 
     # now plot
     edges,weights = zip(*nx.get_edge_attributes(g_sub,'weight').items())
     _,ref = zip(*nx.get_edge_attributes(g_sub,'ref').items())
     weights = np.array(list(weights))
     weights /= np.max(weights)
-    pos = nx.nx_agraph.graphviz_layout(g_sub, prog = 'fdp', args = layout_args)
+    if pos is None:
+        pos = nx.nx_agraph.graphviz_layout(g_sub, prog = 'fdp', args = layout_args)
     for (k, v) in cmap_dict.items():
         edgelist_color = [e for e in edgelist if g_sub.edges[e]['ref'] == k]
         edge_idx_color = np.where([e in edgelist_color for e in g_sub.edges()])[0]
@@ -45,3 +54,20 @@ def draw(g, gene_names, thresh = None, lst = None, cmap_dict = {0 : "MyGrey", 1 
     nx.draw_networkx_nodes(g, pos, nodelist = nodes, node_color = centrality, cmap = matplotlib.colormaps["viridis"], alpha = 0.5, node_size = 600)
     nx.draw_networkx_labels(g, pos, labels = {x : gene_names[x] for (c, x) in zip(centrality, nodes)}, font_size = 14);
     return g_sub
+
+
+def get_union_graph(g_all, gene_names, thresh = 0.99):
+    nodes = []
+    A_all = [np.array(nx.adjacency_matrix(g).todense()) for g in g_all]
+    for g in g_all:
+        g_sub = draw(g, gene_names, thresh = thresh, draw = False)
+        nodes += g_sub.nodes
+    nodes = list(set(nodes))
+    A_agg = np.dstack(A_all).mean(-1)
+    for i in range(A_agg.shape[0]):
+        if i not in nodes:
+            A_agg[i, :] = 0;
+            A_agg[:, i] = 0;
+    g_agg = nx.DiGraph(A_agg)
+    pos = nx.nx_agraph.graphviz_layout(g_agg, prog = "fdp")
+    return g_agg, nodes, pos
