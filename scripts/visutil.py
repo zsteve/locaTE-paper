ef09e7a2880c0d7630fc69e4f05752187e9b0385
Stephen Zhang <syz@syz.id.au>
2023-02-14 16:41:55 +1100

    haem figure

--- scripts/visutil.py
+++ scripts/visutil.py
@@ -3,7 +3,7 @@ import networkx as nx
 import itertools
 import matplotlib
 
-def draw(g, gene_names, thresh = None, node_list = None, edge_list = None, pos = None, cmap_dict = {0 : "MyGrey", 1 : "MyGrey"}, layout_args = "", draw = True):
+def draw(g, gene_names, thresh = None, node_list = None, edge_list = None, pos = None, cmap_dict = {0 : "MyGrey", 1 : "MyGrey"}, layout_args = "", draw = True, kwargs_nodes = {"cmap" : matplotlib.colormaps["viridis"]}, kwargs_edges = {}):
     # get weights from g
     edges,weights = zip(*nx.get_edge_attributes(g,'weight').items())
     weights = np.array(list(weights))
@@ -40,7 +40,7 @@ def draw(g, gene_names, thresh = None, node_list = None, edge_list = None, pos =
         edge_idx_color = np.where([e in edgelist_color for e in g_sub.edges()])[0]
         arrows = nx.draw_networkx_edges(g_sub, pos, 
                                         edgelist = edgelist_color, edge_color = weights[edge_idx_color], alpha = weights[edge_idx_color], 
-                                        width = 2.5*weights[edge_idx_color], edge_cmap = matplotlib.colormaps[v], edge_vmin = -0.1, edge_vmax = 1.1, node_size = 600)
+                                        width = 2.5*weights[edge_idx_color], edge_cmap = matplotlib.colormaps[v], edge_vmin = -0.1, edge_vmax = 1.1, node_size = 600, **kwargs_edges)
         try:
             for a, w in zip(arrows, weights[edge_idx_color]):
                 # from https://stackoverflow.com/questions/67251763/how-to-obtain-non-rounded-arrows-on-fat-lines
@@ -51,12 +51,12 @@ def draw(g, gene_names, thresh = None, node_list = None, edge_list = None, pos =
             pass
     nodes, centrality = zip(*nx.get_node_attributes(g_sub,'centrality').items())
     centrality /= np.max(centrality)
-    nx.draw_networkx_nodes(g, pos, nodelist = nodes, node_color = centrality, cmap = matplotlib.colormaps["viridis"], alpha = 0.5, node_size = 600)
+    nx.draw_networkx_nodes(g, pos, nodelist = nodes, node_color = centrality, alpha = 0.5, node_size = 600, **kwargs_nodes)
     nx.draw_networkx_labels(g, pos, labels = {x : gene_names[x] for (c, x) in zip(centrality, nodes)}, font_size = 14);
     return g_sub
 
 
-def get_union_graph(g_all, gene_names, thresh = 0.99):
+def get_union_graph(g_all, gene_names, thresh = 0.99, layout_args = ""):
     nodes = []
     A_all = [np.array(nx.adjacency_matrix(g).todense()) for g in g_all]
     for g in g_all:
@@ -69,5 +69,5 @@ def get_union_graph(g_all, gene_names, thresh = 0.99):
             A_agg[i, :] = 0;
             A_agg[:, i] = 0;
     g_agg = nx.DiGraph(A_agg)
-    pos = nx.nx_agraph.graphviz_layout(g_agg, prog = "fdp")
+    pos = nx.nx_agraph.graphviz_layout(g_agg, prog = "fdp", args = layout_args)
     return g_agg, nodes, pos
